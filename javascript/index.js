a1d057797b85baaa650895c23dd464c677a6c798
sphathimandla-makhanya <makhanyasm11@gmail.com>
2023-11-22 14:25:44 +0200

    created files

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+// declearing the minute and seconds variables and linking them to the html input tag
+let min = document.querySelector('[data-minutes]')
+let sec = document.querySelector('[data-seconds]')
+
+
+// function for multiplying minutes to seconds, 
+//Takes in a value for minutes
+function toSeconds() {
+    let results = (parseFloat(min.value)*60);
+    sec.value = parseFloat(results.toFixed(2))
+}
+
+// function for dividing seconds to minutes,
+//takes in a value for seconds
+function toMinutes() {
+    let results = (parseFloat(sec.value)/60);
+    min.value = parseFloat(results.toFixed(2))
+}
+
+
+//Added an event listner to the input tag so it can be automatically activated when an input is entered and be displayed in the seconds section
+// 
+min.addEventListener("input", toSeconds);
+
+//Added an event listner to the input tag so it can be automatically activated when an input is entered and be displayed in the minutes section
+sec.addEventListener("input", toMinutes);
